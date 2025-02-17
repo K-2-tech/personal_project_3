5ab2102f14e2f9f80a82421fd13ff9b3cbd25f6b
forkangi3d <takaya20050509@gmail.com>
2025-02-17 14:55:09 +0900

    little change

--- next-project/src/app/tools/learnlooper/page.jsx
+++ next-project/src/app/tools/learnlooper/page.jsx
@@ -4,13 +4,11 @@ import { YouTubeProvider } from "../../contexts/learnlooper/YouTubeContext";
 import MainContainer from "../../components/learnlooper/MainContainer";
 import Header from "../../components/common/Header";
 import Footer from "../../components/common/Footer";
-import DisplayLock from '../../components/displaylock/DisplayLock';
 
 const LL = () => {
   return (
     <>
       <Header />
-      <DisplayLock/>
       <PomodoroProvider>
         <YouTubeProvider>
           <MainContainer />
